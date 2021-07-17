7cb4bf31ee5780e078200ccbbf6f08eb298ce7c5
SamarV-121 <samarvispute121@gmail.com>
2021-07-17 11:18:57 +0530

    m30lte: Compile Keymaster HAL
     * Decommonized from common tree
    
    Change-Id: Ia8f20d9b8de049e5e5d1c23cce58332a6fe7d41c
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- device.mk
+++ device.mk
@@ -5,6 +5,15 @@ $(call inherit-product, device/samsung/universal7904-common/universal7904-common
 
 $(call inherit-product, vendor/samsung/m30lte/m30lte-vendor.mk)
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0-impl \
+    libkeymaster3device
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/android.hardware.keymaster@3.0-service.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vintf/manifest/android.hardware.keymaster@3.0-service.xml
+
 # Rootdir
 PRODUCT_PACKAGES += \
 	fstab.exynos7904 \
